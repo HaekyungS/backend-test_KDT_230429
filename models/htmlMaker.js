9ffb2f5e8895602332b66be82774801a830ca681
HaekyungSeong <haekyungs95@gmail.com>
2023-04-30 00:25:36 +0900

    bodyDiv 에 대해 yes 가 아닌 true로 나옴을 확인하고 정정

--- models/htmlMaker.js
+++ models/htmlMaker.js
@@ -2,32 +2,33 @@ import fs from 'fs'
 
 export default function htmlMaker(answers) {
   const html = `
-    <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>${answers.title}</title>
-        </head>
-        <body>
-          <script>
-            if (${answers.bodyDiv} === 'y') {
-              const div = document.createElement('div')
-              div.setAttribute('id', 'root')
-              document.body.appendChild(div);
-  
-              const p = document.createElement('p')
-              p.innerText = ${answers.pInnerText};
-              div.appendChild(p)
-            } else {
-              const p = document.createElement('p')
-              p.innerText = "${answers.pInnerText}";
-              document.body.appendChild(p)
-            }
-          </script>
-        </body>
-      </html>
-    `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${answers.title}</title>
+  </head>
+  <body>
+    <script>
+      if (${answers.bodyDiv}) {
+        const div = document.createElement('div')
+        div.setAttribute('id', 'root')
+        document.body.appendChild(div);
+
+        const p = document.createElement('p')
+        p.innerText = "${answers.pInnerText}";
+        div.appendChild(p)
+      } else {
+        const p = document.createElement('p')
+        p.innerText = "${answers.pInnerText}";
+        document.body.appendChild(p)
+      }
+    </script>
+  </body>
+</html>`
+
+  // 파일 생성
   fs.writeFileSync(`./result/${answers.fileName}.html`, html);
 }
