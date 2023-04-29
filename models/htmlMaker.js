1649558e2ff01d89f8204591b86b58644e2076e2
HaekyungSeong <haekyungs95@gmail.com>
2023-04-30 00:12:00 +0900

    htmlMaker.js 파일 작성

---
+++ models/htmlMaker.js
@@ -0,0 +1,33 @@
+import fs from 'fs'
+
+export default function htmlMaker(answers) {
+  const html = `
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>${answers.title}</title>
+        </head>
+        <body>
+          <script>
+            if (${answers.bodyDiv} === 'y') {
+              const div = document.createElement('div')
+              div.setAttribute('id', 'root')
+              document.body.appendChild(div);
+  
+              const p = document.createElement('p')
+              p.innerText = ${answers.pInnerText};
+              div.appendChild(p)
+            } else {
+              const p = document.createElement('p')
+              p.innerText = "${answers.pInnerText}";
+              document.body.appendChild(p)
+            }
+          </script>
+        </body>
+      </html>
+    `
+  fs.writeFileSync(`./result/${answers.fileName}.html`, html);
+}
